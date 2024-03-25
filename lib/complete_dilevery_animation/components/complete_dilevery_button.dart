cca89f33dc5ca6bd30223e848af718d2b5f0af0b
Ouchene Mohamed Nour El Islem <150658817+OucheneMohamedNourElIslem658@users.noreply.github.com>
2024-03-25 11:04:06 +0100

    2nd commit

---
+++ lib/complete_dilevery_animation/components/complete_dilevery_button.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+import '../controllers/truck_animation.dart';
+
+class DeliveredButton extends StatelessWidget {
+  const DeliveredButton({
+    super.key,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final truckAnimation = Get.put(TruckAnimation());
+    return FittedBox(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(220),
+        child: Container(
+          height: 220,
+          width: 822,
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            color:Color(0xFF1B262F),
+          ),
+          child: GetBuilder<TruckAnimation>(
+            builder: (_) {
+              if (truckAnimation.completed && !truckAnimation.disableButton) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Order Placed',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'TacticSans',
+                        fontSize: 36
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    SvgPicture.asset('assets/images/checked.svg')
+                  ],
+                );
+              } else {
+                return Stack(
+                  children: [
+                    AnimatedPositioned(
+                      left: -(truckAnimation.rudeLinesTranslation),
+                      top: 0,
+                      bottom: 0,
+                      duration: const Duration(seconds: 2),
+                      child: SvgPicture.asset(
+                        'assets/images/rude_lines.svg',
+                      ),
+                    ),
+                    truckAnimation.showBox
+                    ? AnimatedAlign(
+                      alignment: truckAnimation.boxTranslation.alignment,
+                      duration: truckAnimation.boxTranslation.duration,
+                      curve: truckAnimation.boxTranslation.curve,
+                      child: SvgPicture.asset('assets/images/box.svg')
+                    )
+                    : const SizedBox(),
+                    AnimatedAlign(
+                      alignment: truckAnimation.truckTranslation.alignment,
+                      duration: truckAnimation.truckTranslation.duration,
+                      curve: truckAnimation.truckTranslation.curve,
+                      child: Truck(
+                        light: truckAnimation.light,
+                        openDoors: truckAnimation.openDoors,
+                      )
+                    ),
+                    !truckAnimation.disableButton
+                    ? Positioned.fill(
+                      child: GestureDetector(
+                        onTap: () => truckAnimation.animate(),
+                        child: Container(
+                          color:const Color(0xFF1B262F),
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'Complete',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'TacticSans',
+                              fontSize: 36
+                            ),
+                          ),
+                        ),
+                      )
+                    )
+                    : const SizedBox(),
+                  ],
+                );
+              }
+            }
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Truck extends StatelessWidget {
+  const Truck({
+    super.key,
+    this.light = false,
+    this.openDoors = false
+  });
+
+  final bool light,openDoors;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 370,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AnimatedRotation(
+                turns: openDoors ? 0.27 : 0,
+                alignment: Alignment.topCenter,
+                duration: const Duration(milliseconds: 500),
+                child: SvgPicture.asset('assets/images/door1.svg')
+              ),
+              AnimatedRotation(
+                turns: openDoors ? -0.27 : 0,
+                alignment: Alignment.bottomCenter,
+                duration: const Duration(milliseconds: 500),
+                child: SvgPicture.asset('assets/images/door2.svg')
+              )
+            ],
+          ),
+          if (!light) 
+            SvgPicture.asset('assets/images/truck.svg') 
+          else 
+            SvgPicture.asset('assets/images/truck_light.svg'),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 
-import 'components/truck.dart';
-import 'controllers/truck_animation.dart';
+import 'complete_dilevery_animation/components/complete_dilevery_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,109 +23,9 @@ class Home extends StatelessWidget {
   const Home({super.key});
   @override
   Widget build(BuildContext context) {
-    final truckAnimation = Get.put(TruckAnimation());
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: DeliveredButton(
-          truckAnimation: truckAnimation
-        ),
-      ),
-    );
-  }
-}
-
-class DeliveredButton extends StatelessWidget {
-  const DeliveredButton({
-    super.key,
-    required this.truckAnimation,
-  });
-
-  final TruckAnimation truckAnimation;
-
-  @override
-  Widget build(BuildContext context) {
-    return FittedBox(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(220),
-        child: Container(
-          height: 220,
-          width: 822,
-          alignment: Alignment.center,
-          decoration: const BoxDecoration(
-            color:Color(0xFF1B262F),
-          ),
-          child: GetBuilder<TruckAnimation>(
-            builder: (_) {
-              if (truckAnimation.completed && !truckAnimation.disableButton) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Order Placed',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'TacticSans',
-                        fontSize: 36
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    SvgPicture.asset('assets/images/checked.svg')
-                  ],
-                );
-              } else {
-                return Stack(
-                  children: [
-                    AnimatedPositioned(
-                      left: -(truckAnimation.rudeLinesTranslation),
-                      top: 0,
-                      bottom: 0,
-                      duration: const Duration(seconds: 2),
-                      child: SvgPicture.asset(
-                        'assets/images/rude_lines.svg',
-                      ),
-                    ),
-                    truckAnimation.showBox
-                    ? AnimatedAlign(
-                      alignment: truckAnimation.boxTranslation.alignment,
-                      duration: truckAnimation.boxTranslation.duration,
-                      curve: truckAnimation.boxTranslation.curve,
-                      child: SvgPicture.asset('assets/images/box.svg')
-                    )
-                    : const SizedBox(),
-                    AnimatedAlign(
-                      alignment: truckAnimation.truckTranslation.alignment,
-                      duration: truckAnimation.truckTranslation.duration,
-                      curve: truckAnimation.truckTranslation.curve,
-                      child: Truck(
-                        light: truckAnimation.light,
-                        openDoors: truckAnimation.openDoors,
-                      )
-                    ),
-                    !truckAnimation.disableButton
-                    ? Positioned.fill(
-                      child: GestureDetector(
-                        onTap: () => truckAnimation.animate(),
-                        child: Container(
-                          color:const Color(0xFF1B262F),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            'Complete',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'TacticSans',
-                              fontSize: 36
-                            ),
-                          ),
-                        ),
-                      )
-                    )
-                    : const SizedBox(),
-                  ],
-                );
-              }
-            }
-          ),
-        ),
+        child: DeliveredButton(),
       ),
     );
   }
